f0b06df403a1c09b816c471d6f5fdd02f41df47e
Christoph Finke <ch_finke@web.de>
2021-05-30 14:55:54 +0200

    temporary auth mechanism

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { MapPageComponent } from './map/map-page/map-page.component';
 import { TrackPageComponent } from './tracks/track-page/track-page.component';
 import { NotFoundPageComponent } from './errors/not-found-page/not-found-page.component';
 import { ImprintPageComponent } from './imprint/imprint-page/imprint-page.component';
+import { AuthGuardService as AuthGuard } from './core/auth-guard.service';
 
 const routes: Routes = [
   {
@@ -29,9 +29,8 @@ const routes: Routes = [
     path: '**',
     component: NotFoundPageComponent,
   }
-];
-
-
+].map(r => ({...r, canActivate: [AuthGuard] }));
+// @TODO [remove temporary auth mechanism](https://www.notion.so/remove-temporary-auth-mechanism-6487fe0ba0cb40b4bcabb78a7f51fdea)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
